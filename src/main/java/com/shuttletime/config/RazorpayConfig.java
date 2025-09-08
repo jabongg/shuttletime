c1af835e037cdae66c0f44d508598b2e0c3f904e
jabongg <jbpvns@gmail.com>
2025-09-08 20:43:43 +0530

    SHTL-7: Added razorpay test client secret

--- src/main/java/com/shuttletime/config/RazorpayConfig.java
+++ src/main/java/com/shuttletime/config/RazorpayConfig.java
@@ -10,8 +10,8 @@ public class RazorpayConfig {
     @Bean
     public RazorpayClient razorpayClient() throws Exception {
         // ⚠️ Replace with your Razorpay credentials
-        String keyId = "rzp_test_xxxxx";
-        String keySecret = "your_secret_key";
+        String keyId = "rzp_test_RF9TemS9XUx4ZJ";
+        String keySecret = "bFiwGXqGGI5HKoWWxoU3qdX9";
 
         return new RazorpayClient(keyId, keySecret);
     }
